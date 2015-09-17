ef6561db37ee9dafdecf57c273937f8a8e276593
Eric Mrak <mail@ericmrak.info>
2015-09-16 23:31:49 -0700

    Fixing squeeze in tr

--- src/tr/Tr.hs
+++ src/tr/Tr.hs
@@ -9,7 +9,8 @@ import qualified Pipes.Prelude as P
 import qualified Data.ByteString.Char8 as B
 
 tr :: Args -> IO ()
-tr args = runEffect $ getChars >-> operationFn >-> squeeze (squeezeSet args) >-> putChars where
+tr args = runEffect $ getChars >-> operationFn >-> squeeze set >-> putChars where
+    set = squeezeSet args
     operationFn = case operation args of
                        Translate s -> translate s
                        Delete s -> delete s
@@ -27,12 +28,13 @@ putChars = P.mapM_ putChar
 squeeze :: Monad m => SqueezeSet -> Pipe Char Char m r
 squeeze ss = do
     a <- await
+    yield a
     b <- await
     squeeze' a b where
-    squeeze' x y | x /= y = yield x >> await >>= squeeze' y
-                  | otherwise = if ss ! toIx x
-                                   then await >>= squeeze' x
-                                   else yield x >> await >>= squeeze' y
+    squeeze' l x | x /= l = yield x >> await >>= squeeze' x
+                 | otherwise = if ss ! toIx x
+                                  then await >>= squeeze' x
+                                  else yield x >> await >>= squeeze' x
 
 delete :: DeleteSet -> Pipe Char Char IO ()
 delete s = P.filter (\w -> not (s ! toIx w))
